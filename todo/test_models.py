b777f690663accc6f946d6efb1e5c02f425aa25e
teman67 <>
2023-11-10 12:28:35 +0000

    add testing using django testing

---
+++ todo/test_models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class TestDjango(TestCase):
+
+    def test_this_thing_work(self):
+        self.assertEqual(1, 0)
+
+    def test_this_thing_work2(self):
+        self.assertEqual(1, 1)
